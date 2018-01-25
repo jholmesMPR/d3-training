d92d821e9964fb62b75f007ab8d5849071149c28
Joli Holmes <jholmes@mathematica-mpr.com>
2018-01-25 13:18:38 -0800

    initial boilerplate commit

---
+++ src/materials/Week 5/Assignment/line-chart/js/linechart.js
@@ -0,0 +1,46 @@
+
+function buildChart(containerId) {
+  // size globals
+    var width = 960;
+    var height = 500;
+
+    var margin = {
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50
+    };
+
+    // calculate dimensions without margins
+    var innerWidth = width - margin.left - margin.right;
+    var innerHeight = height - margin.top - margin.bottom;
+
+    // create svg element
+    var svg = d3
+        .select(containerId)
+        .append('svg')
+        .attr('height', height)
+        .attr('width', width);
+
+    // create inner group element
+    var g = svg
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+	// append all of your chart elements to g
+
+// read in our data
+    d3.json('data/climate.json', function(error, data) {
+        // handle read errors
+        if (error) {
+            console.error('failed to read data');
+            return;
+        }
+
+        console.log('raw', data);
+
+    });
+
+}
+
+buildChart('#line-chart')
