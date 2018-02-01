958982be3cdbd5a2ee0cd317eb6ca2ee3870a80e
Joli Holmes <JHolmes@mathematica.net>
2018-02-01 14:18:10 -0800

    initial commit

---
+++ src/materials/Week 6/Assignment/pokemon/js/pokeman.js
@@ -0,0 +1,111 @@
+
+function buildChart(containerId) {
+  // set the dimensions and margins of the graph
+var margin = {
+    top: 50, 
+    right: 50, 
+    bottom: 50, 
+    left: 45}
+
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
+var svg = d3
+        .select(containerId)
+        .append('svg')
+        .attr('height', height)
+        .attr('width', width);
+
+// create inner group element
+var g = svg
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+// read in our data
+    d3.json('data/pokemon.json', function(error, data) {
+        // handle read errors
+        if (error) {
+            console.error('failed to read data');
+            return;
+        }
+        console.log('raw', data);
+
+     //A
+        data.forEach(function(d){
+            d.height_n = d.height.replace(/ m/, '');
+            d.weight_n = d.weight.replace(/ kg/,'');
+            d.egg_n = d.egg.replace(/ km/,'');
+            d.height_n = Number(d.height_n)*3.28084;
+            d.weight_n = Number(d.weight_n)*2.20462;
+            d.egg_n = Number(d.egg_n)*2.20462;
+
+        })
+
+        console.log('clean', data);
+
+        var average_hw = d3.nest()
+          .key(function(d) { return d.name; })
+          .rollup(function(v) { return {
+            avg_height: d3.mean(v, function(d) { return d.height_n; }),
+            avg_weight: d3.mean(v, function(d) { return d.weight_n; })
+          }; })
+          .entries(data);
+        console.log(JSON.stringify(average_hw));
+    //B
+
+    // data.forEach(function(d)){
+    //     if (d.weakness === "Psychic"){
+    //         d.Psychic = 1;
+    //     }
+    //     else{
+    //         d.Psychic = 0;
+    //     }
+    // }
+    var egg_value = d3.nest()
+          .rollup(function(v) { return {
+            egg_sum: d3.sum(v, function(d) { 
+                if (isNaN(d.n_egg)) {
+                    return d.n_egg = -1;
+                }else{
+                    return d.n_egg; }
+
+                })
+              }; 
+            })
+              .entries(data);
+            console.log(JSON.stringify(egg_value));
+
+    //C
+     var weakness = d3.nest()
+      .key(function(d) { return d.name; })
+      .rollup(function(v) { return  v.length; 
+             
+       })
+      .entries(data);
+        console.log(JSON.stringify(weakness));
+
+      console.log('test', data[0].weakness);
+    //D
+
+        var weight_stats = d3.nest()
+          .rollup(function(v) { return {
+            min_weight: d3.min(v, function(d) { return d.weight_n; }),
+            max_weight: d3.max(v, function(d) { return d.weight_n; }),
+            avg_weight: d3.mean(v, function(d) { return d.weight_n; }),
+            p0: d3.quantile(v.map(function(d) { return d.weight_n;}).sort(d3.ascending),0),
+            p25: d3.quantile(v.map(function(d) { return d.weight_n;}).sort(d3.ascending),.25),
+            p50: d3.quantile(v.map(function(d) { return d.weight_n;}).sort(d3.ascending),.5),
+            p75: d3.quantile(v.map(function(d) { return d.weight_n;}).sort(d3.ascending),.75),
+            p100: d3.quantile(v.map(function(d) { return d.weight_n;}).sort(d3.ascending),100)
+         
+          }; })
+          .entries(data);
+        console.log(JSON.stringify(weight_stats));
+
+
+    });
+
+}
+
+
+buildChart('#pokemon')
