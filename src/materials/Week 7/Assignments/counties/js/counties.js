f30df5ad0460563f8ee448e94fae14966ec70288
Joli Holmes <JHolmes@mathematica.net>
2018-02-25 16:27:57 -0800

    added title

--- src/materials/Week 7/Assignments/counties/js/counties.js
+++ src/materials/Week 7/Assignments/counties/js/counties.js
@@ -75,6 +75,8 @@ function buildChart(containerId) {
                             cntys = dm(cnty12, cnty13, cnty14, cnty15, cnty16);
                             console.log('counties', cntys);
 
+                            all = merge(cntys, uscnty);
+                            console.log('all', all);
 
                         });
                     });
@@ -83,15 +85,32 @@ function buildChart(containerId) {
         });
     });
 
-
+    //function dm performs necessary data management steps for the data
     function dm(cnty12, cnty13, cnty14, cnty15, cnty16){
-
+        //Appends data together
         cntys = cnty12.concat(cnty13, cnty14, cnty15, cnty16);
+
+        //Create New Variables
         cntys.forEach(function(d){
             d.id = d.StateCode.concat(d.CountyCode);
             d.pctUE = +d.Percent;
         });
+
         return cntys;
+    }
+
+    function merge(cntys, uscnty){
+     //Merge US Counties with Counties12 - Counties16
+        uscnty.forEach(function(cty) {
+            var result = cntys.filter(function(d) {
+                return d.id === cty.id;
+            });
+        //delete cty.id;
+        //cty.pctUE = (result[0] !== undefined) ? result[0].name : null;
+        });
+
+        return result;
+
     }
 }
 
