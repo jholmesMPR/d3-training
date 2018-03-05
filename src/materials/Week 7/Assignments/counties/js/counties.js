cb53f74c1ac69b5b09bcc5d08efb8b1e372f36dc
Joli Holmes <JHolmes@mathematica.net>
2018-03-04 17:04:27 -0800

    added cholorpleth

--- src/materials/Week 7/Assignments/counties/js/counties.js
+++ src/materials/Week 7/Assignments/counties/js/counties.js
@@ -34,35 +34,35 @@ function buildChart(containerId) {
             console.error('failed to read data');
             return;
         }
-        console.log('county12', cnty12);
+        //console.log('county12', cnty12);
 
         d3.csv('data/laucnty13.csv', function(error, cnty13) {
             if (error) {
                 console.error('failed to read data');
                 return;
             }
-            console.log('county13', cnty13);
+            //console.log('county13', cnty13);
 
             d3.csv('data/laucnty14.csv', function(error, cnty14) {
                 if (error) {
                     console.error('failed to read data');
                     return;
                 }
-                console.log('county14', cnty14);
+                //console.log('county14', cnty14);
 
                 d3.csv('data/laucnty15.csv', function(error, cnty15) {
                     if (error) {
                         console.error('failed to read data');
                         return;
                     }
-                    console.log('county15', cnty15);
+                    //console.log('county15', cnty15);
 
                     d3.csv('data/laucnty16.csv', function(error, cnty16) {
                         if (error) {
                             console.error('failed to read data');
                             return;
                         }
-                        console.log('county16', cnty16);
+                        //console.log('county16', cnty16);
 
 
                         d3.json('data/us-counties.json', function(error, geojson) {
@@ -70,12 +70,12 @@ function buildChart(containerId) {
                                 console.error('failed to read data');
                                 return;
                             }
-                            console.log('geojson', geojson);
+                            //console.log('geojson', geojson);
 
                             data = dm(cnty12, cnty13, cnty14, cnty15, cnty16, geojson);
                             console.log('data', data);
 
-                            chloro(data);
+                            chloro(geojson);
                            
                         });
                     });
@@ -95,50 +95,80 @@ function buildChart(containerId) {
             d.id = +d.StateCode.concat(d.CountyCode);
             d.pctUE = +d.Percent;
         });
-        //console.log('counties', cntys);
+        console.log('counties', cntys);
 
-        var o = [];
-        geojson.features.forEach(function(v) {
-          cntys.forEach(function(w) {
-            if (v.id === w.id) {
-              o.push([v, w]);
-            }
-          });
+        //return cntys;
+
+        var g = [];
+        geojson.features.forEach(function(f) {
+            cntys.forEach(function(d){
+                if(f.id == d.id){
+                    unem = d.pctUE;
+                    year = d.Year;
+                    if (unem) {
+                        f.properties.unem = unem;
+                        f.properties.year = year;
+                    }
+                }
+            });
         });
 
-        //console.log('merge', o);
+        //console.log('unem', g);
+
+        console.log('geojson', geojson);
     }
 
-    // function chloro(cnty, geojson){
-            
-    //     var color = d3.scaleThreshold()
-    //         .domain(d3.extent(cnty, function(d) {
-    //                 return d.pctUE;
-    //             });
-    //         )
-    //         .range(d3.schemeBlues[9]);
+    function chloro(geojson){
 
-    //     var path = d3.geoPath();
+        var Proj = d3
+            .geoAlbersUsa()
+            .translate([innerWidth / 2, innerHeight / 2]);
 
-        
-    //     g.append("g")
-    //       .attr("class", "counties")
-    //      .selectAll("path")
-    //      .data(geojson.features)
-    //      .enter()
-    //      .append("path")
-    //      // .attr("fill", function(d) { return color(d.rate = unemployment.get(d.id)); })
-    //       .attr("d", path)
-    //     .append("title")
-    //     .text("Title");
-    //      // .text(function(d) { return d.rate + "%"; });
+        var geoPath = d3
+            .geoPath()
+            .projection(Proj);
 
-    //   // svg.append("path")
-    //   //     .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-    //   //     .attr("class", "states")
-    //   //     .attr("d", path);
+        var colors = ["#f7fbff",  "#deebf7",  "#c6dbef",  "#9ecae1",  "#6baed6",  "#4292c6",  "#2171b5",  "#08519c",  "#08306b"];
 
-    // }
+        var colorize = d3.scaleThreshold()
+            .domain([2,3, 4, 5, 6, 7, 8, 9, 10])
+            .range(colors);
+
+        //console.log(colorize.range());
+
+        //var colorize = d3.scaleOrdinal(d3.schemeBlues[9]);
+
+
+        g.selectAll("path")
+          .data(geojson.features)
+          .enter()
+          .append("path")
+          .attr("d", geoPath)
+          .attr("class", "feature")
+          .style("fill", "black")
+          .style('stroke', 'white')
+          .style('stroke-width', 0.5);
+
+        // g.append("path")
+        //   .datum(geojson.features)
+        //   .attr("class", "states")
+        //   .attr("d", geoPath);
+
+
+        g.append("g")
+            .attr("class", "counties")
+            .selectAll("path")
+            .data(geojson.features)
+            .enter().append("path")
+            .filter(function(d){
+                return d.properties.year == '2015';
+            })
+              .attr("fill", function(d) { return colorize(d.properties.unem); })
+              .attr("d", geoPath)
+            //.append("title")
+             // .text(function(d) { return d.rate + "%"; })
+        ;
+    }
 
     
 }
