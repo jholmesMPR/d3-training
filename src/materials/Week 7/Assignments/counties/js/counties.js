a106bd5114fd9114d6eeea512af4e29e7d5a4e4e
Joli Holmes <JHolmes@mathematica.net>
2018-03-04 17:44:55 -0800

    added titles across 5 graphs

--- src/materials/Week 7/Assignments/counties/js/counties.js
+++ src/materials/Week 7/Assignments/counties/js/counties.js
@@ -1,35 +1,38 @@
-
 function buildChart(containerId) {
-  // size globals
-    var width = 960;
-    var height = 500;
 
-    var margin = {
-        top: 50,
-        right: 50,
-        bottom: 50,
-        left: 50
-    };
+    var years = ['2012', '2013', '2014', '2015', '2016'];
 
-    // calculate dimensions without margins
-    var innerWidth = width - margin.left - margin.right;
-    var innerHeight = height - margin.top - margin.bottom;
+    years.forEach(function(d, i){
 
-    // create svg element
-    var svg = d3
-        .select(containerId)
-        .append('svg')
-        .attr('height', height)
-        .attr('width', width);
+        var YEAR = years[i];
 
-    // create inner group element
-    var g = svg
-        .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        var width = 850;
+        var height = 500;
+        var margin = {
+            top: 50,
+            right: 50,
+            bottom: 50,
+            left: 50
+        };
 
-	// append all of your chart elements to g
+        // calculate dimensions without margins
+        var innerWidth = width - margin.left - margin.right;
+        var innerHeight = height - margin.top - margin.bottom;
 
-    d3.csv('data/laucnty12.csv', function(error, cnty12) {
+        // create svg element
+        var svg = d3
+            .select(containerId)
+            .append('svg')
+            .attr('height', height)
+            .attr('width', function(d, i){
+                return (i+1)*width + margin.right;
+        });
+
+        var g = svg
+            .append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+            d3.csv('data/laucnty12.csv', function(error, cnty12) {
         if (error) {
             console.error('failed to read data');
             return;
@@ -72,8 +75,8 @@ function buildChart(containerId) {
                             }
                             //console.log('geojson', geojson);
 
-                            data = dm(cnty12, cnty13, cnty14, cnty15, cnty16, geojson);
-                            console.log('data', data);
+                            dm(cnty12, cnty13, cnty14, cnty15, cnty16, geojson);
+                            //console.log('data', data);
 
                             chloro(geojson);
                            
@@ -85,7 +88,7 @@ function buildChart(containerId) {
     });
 
     //function dm performs necessary data management steps for the data
-    function dm(cnty12, cnty13, cnty14, cnty15, cnty16, geojson){
+    function dm(cnty12, cnty13, cnty14, cnty15, cnty16, geojson, YEAR){
 
         //Appends data together
         cntys = cnty12.concat(cnty13, cnty14, cnty15, cnty16);
@@ -97,14 +100,11 @@ function buildChart(containerId) {
         });
         console.log('counties', cntys);
 
-        //return cntys;
-
-        var g = [];
         geojson.features.forEach(function(f) {
             cntys.forEach(function(d){
-                if(f.id == d.id){
+                if((f.id == d.id) & (d.year == YEAR)){
                     unem = d.pctUE;
-                    year = d.Year;
+                    year = d.year;
                     if (unem) {
                         f.properties.unem = unem;
                         f.properties.year = year;
@@ -112,8 +112,6 @@ function buildChart(containerId) {
                 }
             });
         });
-
-        //console.log('unem', g);
 
         console.log('geojson', geojson);
     }
@@ -134,11 +132,6 @@ function buildChart(containerId) {
             .domain([2,3, 4, 5, 6, 7, 8, 9, 10])
             .range(colors);
 
-        //console.log(colorize.range());
-
-        //var colorize = d3.scaleOrdinal(d3.schemeBlues[9]);
-
-
         g.selectAll("path")
           .data(geojson.features)
           .enter()
@@ -149,28 +142,205 @@ function buildChart(containerId) {
           .style('stroke', 'white')
           .style('stroke-width', 0.5);
 
-        // g.append("path")
-        //   .datum(geojson.features)
-        //   .attr("class", "states")
-        //   .attr("d", geoPath);
-
 
         g.append("g")
             .attr("class", "counties")
             .selectAll("path")
             .data(geojson.features)
             .enter().append("path")
-            .filter(function(d){
-                return d.properties.year == '2015';
-            })
+            // .filter(function(d){
+            //     return d.properties.year == '2015';
+            // })
               .attr("fill", function(d) { return colorize(d.properties.unem); })
-              .attr("d", geoPath)
-            //.append("title")
-             // .text(function(d) { return d.rate + "%"; })
-        ;
+              .attr("d", geoPath);
+          
+         g
+            .append('text')
+            .attr('class', 'title')
+            .attr('x', innerWidth / 2)
+            .attr('y', -30)
+            .attr('text-anchor', 'middle')
+            .attr('dominant-baseline', 'baseline')
+            .text(YEAR.concat(' Unemployement Rates'))
+            .style("font", "24px times");
+
     }
 
-    
+
+        //console.log(years[i]);
+
+
+
+    });
+
 }
+
+// function buildChart(containerId, YEAR) {
+//     // size globals
+//     var width = 850;
+//     var height = 500;
+
+//     var margin = {
+//         top: 50,
+//         right: 50,
+//         bottom: 50,
+//         left: 50
+//     };
+
+//     // calculate dimensions without margins
+//     var innerWidth = width - margin.left - margin.right;
+//     var innerHeight = height - margin.top - margin.bottom;
+
+//     // create svg element
+//     var svg = d3
+//         .select(containerId)
+//         .append('svg')
+//         .attr('height', height)
+//         .attr('width', width);
+
+//     // create inner group element
+//     var g = svg
+//         .append('g')
+//         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+// 	// append all of your chart elements to g
+
+//     d3.csv('data/laucnty12.csv', function(error, cnty12) {
+//         if (error) {
+//             console.error('failed to read data');
+//             return;
+//         }
+//         //console.log('county12', cnty12);
+
+//         d3.csv('data/laucnty13.csv', function(error, cnty13) {
+//             if (error) {
+//                 console.error('failed to read data');
+//                 return;
+//             }
+//             //console.log('county13', cnty13);
+
+//             d3.csv('data/laucnty14.csv', function(error, cnty14) {
+//                 if (error) {
+//                     console.error('failed to read data');
+//                     return;
+//                 }
+//                 //console.log('county14', cnty14);
+
+//                 d3.csv('data/laucnty15.csv', function(error, cnty15) {
+//                     if (error) {
+//                         console.error('failed to read data');
+//                         return;
+//                     }
+//                     //console.log('county15', cnty15);
+
+//                     d3.csv('data/laucnty16.csv', function(error, cnty16) {
+//                         if (error) {
+//                             console.error('failed to read data');
+//                             return;
+//                         }
+//                         //console.log('county16', cnty16);
+
+
+//                         d3.json('data/us-counties.json', function(error, geojson) {
+//                             if (error) {
+//                                 console.error('failed to read data');
+//                                 return;
+//                             }
+//                             //console.log('geojson', geojson);
+
+//                             data = dm(cnty12, cnty13, cnty14, cnty15, cnty16, geojson);
+//                             console.log('data', data);
+
+//                             chloro(geojson);
+                           
+//                         });
+//                     });
+//                 });
+//             });
+//         });
+//     });
+
+//     //function dm performs necessary data management steps for the data
+//     function dm(cnty12, cnty13, cnty14, cnty15, cnty16, geojson){
+
+//         //Appends data together
+//         cntys = cnty12.concat(cnty13, cnty14, cnty15, cnty16);
+
+//         //Create New Variables
+//         cntys.forEach(function(d){
+//             d.id = +d.StateCode.concat(d.CountyCode);
+//             d.pctUE = +d.Percent;
+//         });
+//         console.log('counties', cntys);
+
+//         geojson.features.forEach(function(f) {
+//             cntys.forEach(function(d){
+//                 if(f.id == d.id){
+//                     unem = d.pctUE;
+//                     year = YEAR;
+//                     if (unem) {
+//                         f.properties.unem = unem;
+//                         f.properties.year = year;
+//                     }
+//                 }
+//             });
+//         });
+
+//         console.log('geojson', geojson);
+//     }
+
+//     function chloro(geojson){
+
+//         var Proj = d3
+//             .geoAlbersUsa()
+//             .translate([innerWidth / 2, innerHeight / 2]);
+
+//         var geoPath = d3
+//             .geoPath()
+//             .projection(Proj);
+
+//         var colors = ["#f7fbff",  "#deebf7",  "#c6dbef",  "#9ecae1",  "#6baed6",  "#4292c6",  "#2171b5",  "#08519c",  "#08306b"];
+
+//         var colorize = d3.scaleThreshold()
+//             .domain([2,3, 4, 5, 6, 7, 8, 9, 10])
+//             .range(colors);
+
+//         //console.log(colorize.range());
+
+//         //var colorize = d3.scaleOrdinal(d3.schemeBlues[9]);
+
+
+//         g.selectAll("path")
+//           .data(geojson.features)
+//           .enter()
+//           .append("path")
+//           .attr("d", geoPath)
+//           .attr("class", "feature")
+//           .style("fill", "black")
+//           .style('stroke', 'white')
+//           .style('stroke-width', 0.5);
+
+//         // g.append("path")
+//         //   .datum(geojson.features)
+//         //   .attr("class", "states")
+//         //   .attr("d", geoPath);
+
+
+//         g.append("g")
+//             .attr("class", "counties")
+//             .selectAll("path")
+//             .data(geojson.features)
+//             .enter().append("path")
+//             // .filter(function(d){
+//             //     return d.properties.year == '2015';
+//             // })
+//               .attr("fill", function(d) { return colorize(d.properties.unem); })
+//               .attr("d", geoPath)
+//             //.append("title")
+//              // .text(function(d) { return d.rate + "%"; })
+//         ;
+//     }
+   
+// }
 
 buildChart('#counties')
